4415eb27ea99617b2b9acee73e57ceb2e9560f9b
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-03-27 16:06:23 +0100

    add axios

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import axios from "axios";
+const url = "https://wakatime.com/leaders?country_code=CM";
+const AxiosInstance = axios.create();
+AxiosInstance.get(url)
+  .then((response) => {
+    const html = response.data;
+    console.log(html);
+  })
+  .catch(console.error);
