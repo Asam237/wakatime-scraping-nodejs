2e4c2d293ac84d1f96aabc8d9ae0960f17b6f1aa
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-03-27 16:33:24 +0100

    feat: add csv output

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import axios from "axios";
 import cheerio from "cheerio";
 import fs from "fs";
+import { createObjectCsvWriter } from "csv-writer";
 
 const url = "https://wakatime.com/leaders?country_code=CM";
 const AxiosInstance = axios.create();
+const cvWriter = createObjectCsvWriter({
+  path: "./users.csv",
+  header: [
+    { id: "index", title: "Rank" },
+    { id: "programmer", title: "Programmer" },
+    { id: "coded", title: "Hours Coded" },
+    { id: "average", title: "Daily Average" },
+    { id: "languages", title: "Languages Used" },
+  ],
+});
 
 interface IUser {
   index: number;
-  name: string;
+  programmer: string;
   coded: string;
   average: string;
   languages: string;
@@ -20,14 +31,15 @@ AxiosInstance.get(url)
     const usersRow = $(".leaders > tbody > tr");
     const users: IUser[] = [];
     usersRow.each((i, elem) => {
-      const name: string = $(elem).find("td > .leader-coder").text();
+      const programmer: string = $(elem).find("td > .leader-coder").text();
       const coded: string = $(elem).find("td.tcol").text();
       const average: string = $(elem).find("td.avgcol").text();
       const languages: string = $(elem).find("td.langcol").text();
-      users.push({ index: i, name, coded, average, languages });
+      users.push({ index: i, programmer, coded, average, languages });
     });
     console.log(users);
     const jsonContent = JSON.stringify(users);
+    cvWriter.writeRecords(users);
     fs.writeFile("./users.json", jsonContent, "utf8", (error) => {
       if (error) console.log(error);
     });
